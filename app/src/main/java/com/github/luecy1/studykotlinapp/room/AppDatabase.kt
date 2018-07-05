596a604bce60a0ddccdd49e2143e1a2edeeb75a7
luecy1 <kowaheno@gmail.com>
2018-07-05 16:03:32 +0900

    RUNできないのを修正

--- app/src/main/java/com/github/luecy1/studykotlinapp/room/AppDatabase.kt
+++ app/src/main/java/com/github/luecy1/studykotlinapp/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.github.luecy1.studykotlinapp.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [UserDao::class], version = 3)
+@Database(entities = [User::class], version = 3)
 abstract class AppDatabase :RoomDatabase() {
     abstract fun userDao() :UserDao
 }
